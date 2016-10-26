e5d14483eb9a681f1b97639a389c6a95722d8ac2
Jeremy Chow <Jeremy@jeremys-macbook-pro.local>
2016-10-25 22:19:22 -0700

    Initial Commit

---
+++ JeremyVM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JeremyVM
+//
+//  Created by Jeremy Chow on 10/25/16.
+//  Copyright © 2016 Jeremy Chow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
